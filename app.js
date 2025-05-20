e9a92907eefa49c7a628fc3e7ffad5c24a3a0409
inioluwa <samf72302@gmail.com>
2025-05-21 00:13:00 +0100

    sixth commit

--- app.js
+++ app.js
@@ -13,11 +13,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
   console.log("well it did work");
+  console.log("welll im to check if kini");
+
   next();
 });
 
 connectToMongoDb().then(() => {
   server.listen(PORT, () => {
     console.log(`here is the port ${PORT}`);
+    console.log("welll im to confirm stuff");
   });
 });
